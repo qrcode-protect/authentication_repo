b81923fd40ec337c8ed058ec798d82c4a6b160ac
alphaseul <alphaseul@yahoo.fr>
2021-11-09 09:39:17 +0100

    first commit

---
+++ lib/src/authentication_repository.dart
@@ -0,0 +1,96 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:meta/meta.dart';
+import 'models/models.dart';
+
+class AuthenticationRepository {
+  AuthenticationRepository({
+    firebase_auth.FirebaseAuth firebaseAuth,
+    GoogleSignIn googleSignIn,
+  })  : _firebaseAuth = firebaseAuth ?? firebase_auth.FirebaseAuth.instance,
+        _googleSignIn = googleSignIn ?? GoogleSignIn.standard();
+
+  final firebase_auth.FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
+
+  Stream<User> get user {
+    return _firebaseAuth.authStateChanges().map((firebaseUser) {
+      return firebaseUser == null ? User.empty : firebaseUser.toUser;
+    });
+  }
+
+  Future<void> signUp({
+    @required String email,
+    @required String password,
+  }) async {
+    assert(email != null && password != null);
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on Exception {
+      throw SignUpFailure();
+    }
+  }
+
+  Future<void> logInWithGoogle() async {
+    try {
+      await googleAuthentification();
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  Future<void> googleAuthentification() async {
+    final googleUser = await _googleSignIn.signIn();
+    final googleAuth = await googleUser.authentication;
+    final credential = firebase_auth.GoogleAuthProvider.credential(
+      idToken: googleAuth.idToken,
+      accessToken: googleAuth.accessToken,
+    );
+    final result = await _firebaseAuth.signInWithCredential(credential);
+    print('${result.user.displayName}');
+  }
+
+  Future<void> logInWithEmailAndPassword({
+    @required String email,
+    @required String password,
+  }) async {
+    assert(email != null && password != null);
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on Exception {
+      throw LogInWithEmailAndPasswordFailure();
+    }
+  }
+
+  Future<void> logOut() async {
+    try {
+      await Future.wait([
+        _firebaseAuth.signOut(),
+        _googleSignIn.signOut(),
+      ]);
+    } on Exception {
+      throw LogOutFailure();
+    }
+  }
+}
+
+extension on firebase_auth.User {
+  User get toUser {
+    return User(id: uid, email: email, name: displayName, photo: photoURL);
+  }
+}
+
+class SignUpFailure implements Exception {}
+
+class LogInWithEmailAndPasswordFailure implements Exception {}
+
+class LogInWithGoogleFailure implements Exception {}
+
+class LogOutFailure implements Exception {}
---
+++ lib/src/models/models.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+class User extends Equatable {
+  const User({
+    @required this.email,
+    @required this.id,
+    @required this.name,
+    @required this.photo,
+  })  : assert(email != null),
+        assert(id != null);
+
+  final String email;
+
+  final String id;
+
+  final String name;
+
+  final String photo;
+
+  static const empty = User(email: '', id: '', name: null, photo: null);
+
+  @override
+  List<Object> get props => [email, id, name, photo];
+}
